39f62f8e2257e59a4caa8f1bd05c4f7583dec0ed
sjquant <seonujang92@gmail.com>
2023-03-20 02:45:24 +0900

    🚧 Setup use-service composable

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Index page</h1>
+</template>
+
+<script setup lang="ts">
+import { symbols, useService } from "~/composables/use-service";
+
+const service = useService(symbols.auth, "MIKO");
+</script>
