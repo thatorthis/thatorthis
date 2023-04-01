d6e40c1648663d38d6028b8d062f089773640ae6
Seonu Jang <seonujang92@gmail.com>
2023-04-01 23:36:02 +0900

    Implement auth service and google signin (#9)

--- composables/use-service/index.ts
+++ composables/use-service/index.ts
@@ -2,35 +2,36 @@ import { provide, inject } from "vue";
 import { InjectionKey } from "vue";
 import { useAuthService } from "./auth";
 
+type ArgsType<T> = T extends (...args: infer A) => any ? A : never;
+type ReturnTypeOf<T> = T extends (...args: any[]) => infer R ? R : never;
+type CallableTypeOf<T> = (...args: ArgsType<T>) => ReturnTypeOf<T>;
+
 export const symbols = Object.freeze({
-  auth: Symbol("auth") as InjectionKey<typeof useAuthService>,
+  auth: Symbol("auth") as InjectionKey<ReturnTypeOf<typeof useAuthService>>,
 });
 
 const serviceFuncMap: Map<InjectionKey<any>, (...args: any[]) => any> = new Map(
   [[symbols.auth, useAuthService]]
 );
 
-export function useService<T extends (...args: any[]) => any>(
-  symbol: InjectionKey<T>,
-  ...args: Parameters<T>
-): T {
+export function useService<U>(
+  symbol: InjectionKey<U>,
+  ...args: Parameters<CallableTypeOf<U>>
+): U {
   let service = inject(symbol, null);
   if (!service) {
-    service = pickService(symbol, ...args) as T;
+    const service = pickService(symbol)(...args) as U;
     provide(symbol, service);
+    return service;
   }
   return service;
 }
 
-function pickService<T extends (...args: any[]) => any>(
-  symbol: InjectionKey<T>,
-  ...args: Parameters<T>
-): T {
+function pickService<U>(symbol: InjectionKey<U>): CallableTypeOf<U> {
   const serviceFunc = serviceFuncMap.get(symbol);
 
   if (!serviceFunc) {
     throw new Error("Unknown service");
   }
-
-  return serviceFunc(...args);
+  return serviceFunc;
 }
