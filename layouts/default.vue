f7e01da102adea46fde3d81570924166c0640b02
sjquant <seonujang92@gmail.com>
2023-04-06 00:46:17 +0900

    💄 Add default layout and base css

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="default-layout">
+    <div class="default-layout__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.default-layout {
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 1.6rem 1.6rem;
+
+  &__content {
+    max-width: 76.8rem;
+    width: 100%;
+  }
+}
+</style>
