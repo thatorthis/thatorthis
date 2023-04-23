258bf99c031f360ee8a062bcca7acfaee227be13
sjquant <seonujang92@gmail.com>
2023-04-23 15:57:11 +0900

    💄  Create profile icon

---
+++ components/ProfileIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- Profile Icon -->
+  <div class="profile-icon">
+    <img src="~/assets/images/meerkat-profile.png" alt="Profile Icon" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.profile-icon {
+  img {
+    width: 4rem;
+    border-radius: 100%;
+    object-fit: cover;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="default-layout">
+    <header class="default-layout__header">
+      <div>
+        <!-- Logo -->
+      </div>
+      <div class="default-layout__header__profile">
+        <profile-icon />
+      </div>
+    </header>
     <div class="default-layout__content">
       <slot />
     </div>
   </div>
 </template>
+<script setup lang="ts">
+import ProfileIcon from "~/components/ProfileIcon.vue";
+</script>
 
 <style lang="scss" scoped>
 .default-layout {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   height: 100vh;
   padding: 1.6rem 1.6rem;
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
 
   &__content {
     max-width: 76.8rem;
