e8e8d616c1bf7e69ae47cd357533cd48033b8fd1
Seonu Jang <seonujang92@gmail.com>
2023-05-06 16:10:52 +0900

    💄 Design change (#25)

---
+++ components/OptionCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <div :class="['option-card', { selected }]">
+    <img
+      :src="`https://picsum.photos/400?random=${Math.round(
+        Math.random() * 10
+      )}`"
+      alt="option image"
+    />
+    <div class="text">
+      <span>{{ props.option.option }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Option } from "~~/daos";
+const props = defineProps<{
+  option: Option;
+  selected: boolean;
+}>();
+</script>
+
+<style scoped lang="scss">
+.option-card {
+  border-radius: 5px;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  width: 100%;
+  text-align: left;
+  color: $grey-800;
+  cursor: pointer;
+
+  &.selected {
+    background-color: $grey-200;
+
+    .text {
+      font-weight: bold;
+    }
+  }
+
+  &:hover:not(.selected) {
+    background-color: $grey-100;
+
+    .text {
+      font-weight: bold;
+    }
+  }
+
+  img {
+    width: 100%;
+    aspect-ratio: 16/9;
+    object-fit: cover;
+    margin-bottom: 1rem;
+  }
+
+  .text {
+    font-size: 1.6rem;
+  }
+}
+</style>
